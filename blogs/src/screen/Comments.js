bff53d06971c6ba93dccdcbec8242bbf6b1911f3
Ranjan <ranjanrajthala24@gmai.com>
2021-06-23 22:08:33 +0545

    implementing comment section logic

--- blogs/src/screen/Comments.js
+++ blogs/src/screen/Comments.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import commentsDt from "../data/commentsDt";
 import { BsHeart } from "react-icons/bs";
+import { BsHeartFill } from "react-icons/bs";
 import { BiShareAlt } from "react-icons/bi";
 import { RiSendPlaneFill } from "react-icons/ri";
 import { BsReplyAllFill } from "react-icons/bs";
@@ -8,28 +9,89 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 import "./Comments.css";
 
 const Comments = (props) => {
-  const { name, img, comment_text, date } = props;
+  const [isShowHeart, isSetShowHeart] = useState(true);
+  const [isShowReply, isSetShowReply] = useState(false);
+  const [isShowThreeDots, isSetShowThreeDots] = useState(false);
+  const {
+    name = "dummy person",
+    img = "/uploads/7.jpg",
+    comment_text,
+    date = "5/june/2021",
+  } = props;
   return (
     <div className="comment-container">
       <h3 className="comment-person">{name}</h3>
       <p className="comment-date">{date}</p>
       <img src={img} className="comment-img" alt="image" />
       <p className="comment-text">{comment_text}</p>
-      <BsHeart className="comment-heart" />
-      <BsReplyAllFill className="comment-reply" />
-      <BsThreeDotsVertical className="comment-three-dots" />
+      <BsHeart
+        className={isShowHeart ? "comment-heart" : "hide-comment-heartFill"}
+        onClick={() => isSetShowHeart(!isShowHeart)}
+      />
+      <BsHeartFill
+        className={isShowHeart ? "hide-comment-heartFill" : "comment-heartFill"}
+        onClick={() => isSetShowHeart(!isShowHeart)}
+      />
+      <BsReplyAllFill
+        className="comment-reply"
+        onClick={() => isSetShowReply(!isShowReply)}
+      />
+      <BsThreeDotsVertical
+        className="comment-three-dots"
+        onClick={() => isSetShowThreeDots(!isShowThreeDots)}
+      />
+
+      {/* for toggle reply */}
+      <div
+        className={
+          isShowReply
+            ? "comment-reply-container"
+            : "hide-comment-reply-container"
+        }
+      >
+        <input
+          placeholder="write your reply"
+          className="comment-input-reply"
+        ></input>
+        <RiSendPlaneFill
+          className="comment-send-reply"
+          onClick={() => console.log("hello ranjan")}
+        />
+      </div>
+
+      {/* for toggle three dots */}
+
+      <div
+        className=""
+        className={
+          isShowThreeDots
+            ? "comment-three-dots-container"
+            : "hide-comment-three-dots-container"
+        }
+      >
+        <p>Report this comment</p>
+        <p>View profile</p>
+        <p>Reply comment</p>
+      </div>
     </div>
   );
 };
 
 const CommentList = () => {
-  const [persons, setPersons] = useState([]);
+  const [text, setText] = useState("");
+  const [comments, setComments] = useState(commentsDt);
 
-  const newList = commentsDt.map((comment) => {
-    return <Comments key={comment.id} {...comment} />;
+  const newList = comments.map((comment, index) => {
+    return <Comments key={index} {...comment} />;
   });
   const handleSubmit = (e) => {
-    console.log("hello world");
+    if (text) {
+      const commentText = { comment_text: text };
+      setText("");
+      setComments((comments) => {
+        return [...comments, commentText];
+      });
+    }
   };
   return (
     <>
@@ -38,8 +100,8 @@ const CommentList = () => {
           <input
             placeholder="write your comment"
             className="comment-input"
-            value={persons}
-            onChange={(e) => setPersons(e.value.target)}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
           ></input>
           <RiSendPlaneFill className="comment-send" onClick={handleSubmit} />
         </div>
