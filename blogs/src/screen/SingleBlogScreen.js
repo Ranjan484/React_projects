9bfd6a922ea065e0151377ee2891cd2c8a7373ec
Ranjan <ranjanrajthala24@gmai.com>
2021-06-28 08:56:04 +0545

    added some css in singleblogs

--- blogs/src/screen/SingleBlogScreen.js
+++ blogs/src/screen/SingleBlogScreen.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { RiDoubleQuotesR } from "react-icons/ri";
 import CardInfos from "../data/cardInfo";
 import RelatedBlogCardList from "../components/RelatedBlogs";
 import "./singleBlog.css";
@@ -22,19 +23,26 @@ const SingleBlog = ({ match }) => {
           We’re living in the era of technology, and technological growth is
           nowhere near stopping. While one of the paths to becoming a programmer
           is to earn a computer science degree, you can still become one without
-          a degree. Many programming jobs don’t require a computer science or an
-          IT degree; instead, they’re interested in your coding abilities. This
-          means that even though a degree would be useful and open several doors
-          for you, it isn’t a must.
+          a degree.
+          <strong style={{ background: "#34d399" }}>
+            {" "}
+            Many programming jobs don’t require a computer science or an IT
+            degree; instead, they’re interested in your coding abilities.{" "}
+          </strong>{" "}
+          This means that even though a degree would be useful and open several
+          doors for you, it isn’t a must.
         </p>
         <p className="paragraph">
-          However, there are other options other than a degree, that could
-          potentially help you land a job. For instance, if you were to get a
-          software development or cybersecurity diploma, it would certainly look
-          good in the eyes of the hiring manager. The same goes for a
-          certificate too; if your CV has relevant certificate programs in it,
-          such as network security management for example, that along with your
-          skills will leave a good impression.
+          <div className="sb-highlight">
+            {/* <RiDoubleQuotesL /> */}
+            However, if you were to get a software development or cybersecurity
+            diploma, it would certainly look good in the eyes of the hiring
+            manager. The same goes for a certificate too; if your CV has
+            relevant certificate programs in it, such as network security
+            management for example, that along with your skills will leave a
+            good impression.
+            {/* <RiDoubleQuotesR /> */}
+          </div>
         </p>
         <div>
           <img src={blog.body_img} alt="sec-img" className="body-img" />
@@ -42,12 +50,14 @@ const SingleBlog = ({ match }) => {
         <h2 className="blog-heading">Computer in every sector</h2>
         <p className="paragraph">
           However, there are other options other than a degree, that could
-          potentially help you land a job. For instance, if you were to get a
-          software development or cybersecurity diploma, it would certainly look
-          good in the eyes of the hiring manager. The same goes for a
-          certificate too; if your CV has relevant certificate programs in it,
-          such as network security management for example, that along with your
-          skills will leave a good impression.
+          potentially help you land a job. For instance,{" "}
+          <strong style={{ background: "#34d399" }}>
+            if you were to get a software development or cybersecurity diploma,
+            it would certainly look good in the eyes of the hiring manager
+          </strong>
+          . The same goes for a certificate too; if your CV has relevant
+          certificate programs in it, such as network security management for
+          example, that along with your skills will leave a good impression.
         </p>
 
         <p className="paragraph">
@@ -77,19 +87,24 @@ const SingleBlog = ({ match }) => {
           Programming is a skill that comes with numerous benefits. Some of the
           advantages that come withIn the world of programming, it is often said
           that coding is all about solving problems, and computer programs
-          should solve problems for us. Learning programming is more or less
-          coding and determining how to tackle the issues that arise during
-          coding and finding solutions for them. The more you get to solve these
-          problems, the more practical it becomes. You might even start applying
-          this problem-solving mindset in different aspects of your professional
-          life and not only while coding.
+          should solve problems for us.
+          <strong style={{ background: "#34d399" }}>
+            {" "}
+            Learning programming is more or less coding and determining how to
+            tackle the issues that arise during coding and finding solutions for
+            them
+          </strong>
+          . The more you get to solve these problems, the more practical it
+          becomes. You might even start applying this problem-solving mindset in
+          different aspects of your professional life and not only while coding.
         </p>
 
         <p className="paragraph">
           knowing code are a guaranteed job, getting to create interesting
           applications, developing problem-solving skills, and sometimes even
           the opportunity to work from home. We hope we convinced you to start
-          learning to code. Happy coding!
+          learning to code.
+          <strong style={{ textDecoration: "italic" }}> Happy coding!</strong>
         </p>
         <ul className="blog-icons">
           <li>
