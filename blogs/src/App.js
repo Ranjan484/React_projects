6ec3f3c3c309807e34ca3e05f31fc0f7f749ccd5
Ranjan <ranjanrajthala24@gmai.com>
2021-07-09 13:45:05 +0545

    birthday reminder

--- blogs/src/App.js
+++ blogs/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
 
 const App = () => {
-  return <>hello world</>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}> clear all</button>
+      </section>
+    </>
+  );
 };
 
 export default App;
