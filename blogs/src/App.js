a96f601bf06ea05e2636aaaa0a03444536c58a0b
Ranjan <ranjanrajthala24@gmai.com>
2021-07-09 12:54:03 +0545

    starting new project

--- blogs/src/App.js
+++ blogs/src/App.js
@@ -1,19 +1,7 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
 
-import Home from "./screen/homeScreen/Home_screen";
-import SingleBlog from "./screen/singleBlog/SingleBlogScreen";
-import ScrollToTop from "./components/ScrollToTop";
-
-function App() {
-  return (
-    <Router>
-      <ScrollToTop />
-      <Switch>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/single-blogs/:id" exact component={SingleBlog}></Route>
-      </Switch>
-    </Router>
-  );
-}
+const App = () => {
+  return <>hello world</>;
+};
 
 export default App;
--- blogs/src/index.js
+++ blogs/src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-// import "./index.css";
-// import "./singleBlog.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
